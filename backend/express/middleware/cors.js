239bc35750ace4fd15ccfd2a4193076329c2c77a
Yassine <yassinejedidi17@gmail.com>
2025-07-07 15:16:10 +0100

    edit cors to allow expo

--- backend/express/middleware/cors.js
+++ backend/express/middleware/cors.js
@@ -7,21 +7,37 @@ dotenv.config();
  * CORS middleware configuration
  */
 const corsMiddleware = () => {
+  const allowedOrigins = [
+    process.env.FRONTEND_URL,
+    "http://localhost:8081",
+    "http://localhost:3000", // (optional) for web dev
+  ];
   const corsOptions = {
-    origin: process.env.FRONTEND_URL,
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps or curl)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     optionsSuccessStatus: 200,
   };
-
   return cors(corsOptions);
 };
 
 module.exports = {
   corsMiddleware,
   corsOptions: () => ({
-    origin: process.env.FRONTEND_URL,
+    origin: [
+      process.env.FRONTEND_URL,
+      "http://localhost:8081",
+      "http://localhost:3000",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
